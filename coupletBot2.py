bbc4741f931f05b8a9ee89c9fea01a5b4003b7ac
Alex Chow <czolbe88@gmail.com>
2017-11-07 14:46:34 +0800

    test files

--- coupletBot2.py
+++ coupletBot2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import praw
 import couplet
 import couplet2
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 with open("test.txt", "a") as f:
     f.write("hello world")
